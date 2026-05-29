e5b920f601f9acf38e760cfd159a80f1b7a85a2d
mathi <mathiaslucht6@gmail.com>
2026-05-29 23:47:31 +0200

    pre

--- src/main/scala/skald/model/Command.scala
+++ src/main/scala/skald/model/Command.scala
@@ -17,6 +17,8 @@ enum Command:
   case Export(name: String, value: String)
 
   case External(name: String, args: List[String])
+  case Interactive(name: String, args: List[String])
+
   case Pipeline(commands: List[Command])
   case Redirect(cmd: Command, target: RedirectionType, mode: RedirectionMode, targetFile: String)
   case Subprocess(cmd: Command)
