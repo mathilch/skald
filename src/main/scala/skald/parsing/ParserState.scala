8ac35e8978111c4b6ae51461f029dd94aa268a87
mathi <mathiaslucht6@gmail.com>
2026-05-29 23:53:18 +0200

    simple parser state

---
+++ src/main/scala/skald/parsing/ParserState.scala
@@ -0,0 +1,19 @@
+package skald
+
+import Result._
+
+case class ParserState(tokens: List[String]) {
+  def peek: Option[String] = tokens.headOption 
+
+  def next: ParserState = ParserState(tokens.drop(1))
+
+  def isEmpty: Boolean = tokens.isEmpty
+
+  def expect(expected: String, errorMsg: String): Result[ParseError, Unit] = {
+    peek match {
+      case Some(t) if t == expected => Success(())
+      case Some(t) => Fail(ParseError.InvalidSyntax(s"$errorMsg (found '$t')"))
+      case None    => Fail(ParseError.InvalidSyntax(s"$errorMsg (reached EOF)"))
+    }
+  }
+}
